04faee46c6b02e4fd7691cf81d73e7731db72321
ALIPAT <23010749@comlabserver.csabcomlab.edu.ph>
2024-09-05 17:31:04 +0800

    add new files

--- Payroll.php
+++ Payroll.php
@@ -1,28 +1,29 @@
-<? php 
+<?php 
 
 	class Payroll {
 
 		private string $firstname = "Emzy";
-		private string $lastname; = "Cute";
+		private string $lastname = "Cute";
 
 		private int $tax = 50;
 		private int $salary = 500;
 		
 
-		public function computeNet(); float {
-				$net = $this->salary - $this->tax;
+		public function computeNet() : float {
+			$net = $this->salary - $this->tax;
 			return $net;
 		}
-		public function getEmployeeName(); string {
-				return $this->firstname. ", ".$this->lastname;
+		
+		public function getEmployeeName() : string {
+			return $this->firstname. ", ".$this->lastname;
 
 		}
 
-	}
-	
-	$computeNet = new computeNet ();	
+		}
 
-	echo "First Name:". $Payroll->getEmployeeName() . "<br/>";
-	echo "Net:". $Payroll->computeNet();
+	$Payroll = new Payroll ();	
+
+	echo "First Name: ". $Payroll->getEmployeeName() . "<br/>";
+	echo "Net: ". $Payroll->computeNet();
 
 ?>
